201e80380c6ca692709502ae4b819c6a66fc2dcc
Josemaria Cerviño <pity@MacBook-Pro-de-Josemaria.local>
2021-03-08 10:01:17 -0300

    Se realizo la funcionalidad del formulario y ventanas modales correspondientes  en el archivo js -> contact-form

--- js/script.js
+++ js/script.js
@@ -4,17 +4,49 @@
     const $btnMenu = d.querySelector(".menu-btn"),
         $menu = d.querySelector(".menu");
 
-    $btnMenu.addEventListener("click",(e) => {
+    $btnMenu.addEventListener("click", (e) => {
         $btnMenu.firstElementChild.classList.toggle("none");
         $btnMenu.lastElementChild.classList.toggle("none");
         $menu.classList.toggle("is-active");
     });
 
     d.addEventListener("click", (e) => {
-        if(!e.target.matches(".menu a")) return false;
+        if (!e.target.matches(".menu a")) return false;
 
         $btnMenu.firstElementChild.classList.remove("none");
         $btnMenu.lastElementChild.classList.add("none");
         $menu.classList.remove("is-active");
+    });
+})(document);
+
+/******** Contact-Form **********/
+
+((d) => {
+    const $form = d.querySelector(".contact-form");
+    const $loader = d.querySelector(".contact-form-loader");
+    const $response = d.querySelector(".contact-form-response");
+
+    $form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/josemariacervinio@gmail.com", {
+                method: "POST",
+                body: new FormData(e.target)
+            }).then((res) => (res.ok ? res.json() : Promise.reject(res)))
+            .then(json => {
+                console.log(json);
+                location.hash = "#gracias";
+                $form.reset();
+            })
+            .catch(err => {
+                console.log(err);
+                let message = err.statusText || "No se pudo enviar, intenta nuevamente"
+                $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+            }).finally(() => {
+                $loader.classList.add("none");
+                setTimeout(() => {
+                    location.hash = "#close";
+                }, 3000);
+            });
     });
 })(document);
